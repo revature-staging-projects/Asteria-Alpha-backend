ab4e5b8a07996367e366fa2fc90955eb900a7ffa
chris <armoredmoose@gmail.com>
2021-05-02 17:02:58 -0500

    added stuff

--- src/main/java/com/revature/AsteriaAlpha.java
+++ src/main/java/com/revature/AsteriaAlpha.java
@@ -3,19 +3,21 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.annotations.ApiIgnore;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class AsteriaAlpha {
     public static void main(String[] args) {
         SpringApplication.run(AsteriaAlpha.class, args);
     }
-
 
     @Bean
     public Docket api() {
