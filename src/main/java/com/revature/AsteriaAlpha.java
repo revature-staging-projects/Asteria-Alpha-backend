728d2613e26c4fddee46cd9fee29d53247e6c563
chris <armoredmoose@gmail.com>
2021-05-02 15:26:47 -0500

    added swagger support

--- src/main/java/com/revature/AsteriaAlpha.java
+++ src/main/java/com/revature/AsteriaAlpha.java
@@ -2,11 +2,30 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 
 @SpringBootApplication
 public class AsteriaAlpha {
     public static void main(String[] args) {
         SpringApplication.run(AsteriaAlpha.class, args);
+    }
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.revature.controllers"))
+                // Ignore the root controller
+                .apis(RequestHandlerSelectors.withClassAnnotation(ApiIgnore.class).negate())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
