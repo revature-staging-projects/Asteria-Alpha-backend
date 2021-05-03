beb3220ab2e8e766e39dfaa124432cef34d60bf9
chris <armoredmoose@gmail.com>
2021-05-03 11:48:37 -0500

    updated epic image fav repo

--- src/main/java/com/revature/repositories/FavEPICRepo.java
+++ src/main/java/com/revature/repositories/FavEPICRepo.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface FavEPICRepo extends CrudRepository<FavEpicImage,Integer> {
 
-        @Query(value = "select * from Fav_Epic_Image \n" +
+        @Query(value = "select fav.id,fav.title,fav.image_date,fav.caption from Fav_Epic_Image as fav \n" +
                 "join  Fav_Epic_Ref as refs on id = refs.img_id\n" +
                 "join Users as users on refs.user_id = users.id where users.username = :username",nativeQuery = true)
         List<FavEpicImage> getUserFavoriteImages(final String username);
@@ -22,6 +22,15 @@ public interface FavEPICRepo extends CrudRepository<FavEpicImage,Integer> {
         @Query(value = "insert into fav_epic_ref (user_id,img_id)\n" +
                 " select u.id,img.id from users as u, fav_epic_image as img where u.username =:username and img.image_date = :date", nativeQuery = true)
         void updateRefTable(final String username,final String date);
+
+
+
+       @Modifying
+       @Transactional
+       @Query(value = "INSERT INTO Fav_Epic_Image (title,image_date,caption)" +
+               "SELECT e.title,e.image_dat,e.caption from EPIC_Image where e.id = 1",nativeQuery = true)
+        void addImageToFav();
+
 
         List<FavEpicImage> findByImage(final String title);
 }
