8c72f3d16bd8e4da0ad2c9fec60e6acd2f8234bc
chris <armoredmoose@gmail.com>
2021-05-02 22:07:28 -0500

    updated the update sql query

--- src/main/java/com/revature/repositories/UserRepo.java
+++ src/main/java/com/revature/repositories/UserRepo.java
@@ -13,22 +13,20 @@ import java.util.List;
 public interface UserRepo extends CrudRepository<User,Integer> {
 
 
-    @Query(value = "select v.verified from Verified as v\n" +
-            "join Users as u on u.id = v.user_id\n" +
-            "where u.username = :username", nativeQuery = true)
+    @Query(value = "SELECT v.verified from Verified as v\n" +
+            "JOIN Users as u on u.id = v.user_id\n" +
+            "WHERE u.username = :username", nativeQuery = true)
     boolean getIfValid(final String username);
 
     @Transactional
     @Modifying
-    @Query(value = "insert into verified (user_id)" +
-                    "Select u.id from users as u where u.username = :username", nativeQuery = true)
+    @Query(value = "INSERT INTO Verified (user_id)" +
+                    "SELECT u.id from Users as u WHERE u.username = :username", nativeQuery = true)
     void addToVerifiedTable(final String username);
 
     @Transactional
     @Modifying
-    @Query(value = "update verified" +
-            "set verified = true" +
-            "where user_id = :id")
+    @Query(value = "UPDATE Verified SET verified = true WHERE user_id = :id",nativeQuery = true)
     void updateVerified(final int id);
 
 
