de9c873c19de8de791b0fbb0f78d05a8eca96fcb
chris <armoredmoose@gmail.com>
2021-05-04 00:28:49 -0500

    made some changes

--- src/main/java/com/revature/models/news/Thumbnail.java
+++ src/main/java/com/revature/models/news/Thumbnail.java
@@ -1,5 +1,7 @@
 package com.revature.models.news;
 
+import java.util.Objects;
+
 public class Thumbnail {
 
     private String thumbnail;
@@ -11,5 +13,18 @@ public class Thumbnail {
 
     public void setThumbnail(final String thumbnail) {
         this.thumbnail = thumbnail;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Thumbnail thumbnail1 = (Thumbnail) o;
+        return thumbnail.equals(thumbnail1.thumbnail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(thumbnail);
     }
 }
